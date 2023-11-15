04edeafc13b0b73d76ded51e5831709ff353ad91
hannahbnelson <hnelson2@nd.edu>
2023-11-15 06:09:13 -0500

    rename test_plotter.py

--- mc_validation/plotter.py
+++ mc_validation/plotter.py
@@ -1,82 +1,36 @@
-import os
-import pickle
-import gzip
-import argparse
-import matplotlib.pyplot as plt
-import topcoffea.modules.utils as utils
-
+import pickle #read pickle file
+import coffea
 from coffea import hist
-from topcoffea.modules.HistEFT import HistEFT
+import topcoffea.modules.HistEFT as HistEFT
+import topcoffea.modules.eft_helper as efth
+import gzip #read zipped pickle file
+import matplotlib.pyplot as plt #plot histograms
+import mplhep as hep
+import numpy as np
 
+hep.style.use("CMS")
+params = {'axes.labelsize': 20,
+          'axes.titlesize': 20,
+          'legend.fontsize':20}
+plt.rcParams.update(params)
 
-WCPT_EXAMPLES = {
-    "nonsm": {
-        "ctq1": 1.5, 
-        "cQd1": 1.5,
-        "ctu1": 1.5,
-        "cQu1": 1.5,
-        "cQq11": 1.5, 
-        "cQq13": 1.5,
-        "cQq83": 1.5, 
-        "ctG": 1.5, 
-        "cQu8": 1.5, 
-        "ctd1": 1.5, 
-        "ctd8": 1.5, 
-        "cQq81": 1.5, 
-        "ctu8": 1.5, 
-        "cQd8": 1.5, 
-        "ctq8": 1.5,
-    },
-    "sm": {
-        "ctq1": 0.0,
-        "cQd1": 0.0,
-        "ctu1": 0.0,
-        "cQu1": 0.0,
-        "cQq11": 0.0,
-        "cQq13": 0.0,
-        "cQq83": 0.0,
-        "ctG": 0.0,
-        "cQu8": 0.0,
-        "ctd1": 0.0,
-        "ctd8": 0.0,
-        "cQq81": 0.0,
-        "ctu8": 0.0,
-        "cQd8": 0.0,
-        "ctq8": 0.0,
-    }
-}
+fin = 'with_mtt.pkl.gz'
+hists = {}
 
-# Takes a hist with one sparse axis and one dense axis, overlays everything on the sparse axis
-def make_single_fig(histo):
-    fig, ax = plt.subplots(1, 1, figsize=(7,7))
-    histo.plot1d(
-        stack=False,
-    )
-    ax.autoscale(axis='y')
-    plt.legend()
-    return fig
+with gzip.open(fin) as fin: 
+    hin = pickle.load(fin)
+    for k in hin.keys():
+        if k in hists: 
+            hists[k]+=hin[k]
+        else: 
+            hists[k]=hin[k]
 
+        print(hists[k])
 
-def main():
+h = hists['mtt_ordered']
+# print(h.axes())
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input_histo_file")
-    args = parser.parse_args()
-
-    histo_in = args.input_histo_file
-
-    # Get the histograms
-    hin_dict = pickle.load(gzip.open(histo_in))
-
-    # Grab the one we want to plot
-    variable = "tops_pt"
-    histo = hin_dict[variable]
-
-    h = histo.integrate('tops_pt').values()
-    fig, ax = plt.subplots(1,1, figsize=(7,7)) #create an axis for plotting
-    hist.plot1d(h, stack=True)
-    plt.savefig("test_fig.pdf")
-
-
-main()
-
+fig, ax = plt.subplots(1,1) #create an axis for plotting
+hist.plot1d(h, ax=ax, stack=True)
+ax.legend()
+fig.savefig('mtt_ordered.pdf')
