4a88fd5e35e7e2d9104dc2870c722845e3839941
hannahbnelson <hnelson2@nd.edu>
2025-02-18 16:52:56 -0500

    scripts and cards to run MG stand alone at different WC points

---
+++ cards/makeStandAloneCards/makeStandAlonecard_tW.py
@@ -0,0 +1,47 @@
+import os
+import json
+import argparse
+import random
+
+def make_card_frozen(MG_dir, wc_names, scan_points, outname):
+    
+    rwgtCards = ''
+    rwgtCards += f"launch {MG_dir} \n"
+
+    rwgtCards += f"launch -n SM \n"
+    for wc in wc_names: 
+        rwgtCards += f"set param_card {wc} 0.0 \n"
+
+    for wc in wc_names:
+        for i in scan_points: 
+            if wc=='ctGRe' or wc=='ctGIm':
+                i = i/10.0
+            rwgtCards += f"launch -n {wc}={i}\n"
+
+            for j in wc_names: 
+                if wc == j: 
+                    rwgtCards += f"set param_card {j} {i} \n"
+                else: 
+                    rwgtCards += f"set param_card {j} 0.0 \n"
+
+    fname = f"{outname}.dat"
+    open(fname, 'wt').write(rwgtCards)
+    print(f"card saved to: {fname}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Command Line options parser")
+    parser.add_argument("jsonFile", help="Json that contains settings")
+    parser.add_argument("--outname", '-o', default="standAloneCard", help="output file name")
+    args = parser.parse_args()
+
+    jsonFile = args.jsonFile
+    outname = args.outname
+
+    with open(jsonFile) as jf: 
+        options = json.load(jf)
+
+    MG_dir = options['MG_dir']
+    wc_names = options['wc_names']
+    scan_points = options['scan_points']
+
+    make_card_frozen(MG_dir, wc_names, scan_points, outname)
