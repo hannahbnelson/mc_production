e419799449e7f27d375ffc2b27fb374c6db20d4f
hannahbnelson <hnelson2@nd.edu>
2025-02-19 17:36:05 -0500

    add function to make card for profiled case

--- cards/makeStandAloneCards/makeStandAloneCard.py
+++ cards/makeStandAloneCards/makeStandAloneCard.py
@@ -3,7 +3,36 @@ import json
 import argparse
 import random
 
+def make_card_profiled(MG_dir, wc_names, scan_points, outname, profiled_points):
+    print("Creating card with WCs profiled: \n", profiled_points, '\n')
+
+    rwgtCards = ''
+    rwgtCards += f"launch {MG_dir} \n"
+
+    rwgtCards += f"launch -n SM \n"
+    for wc in wc_names: 
+        rwgtCards += f"set param_card {wc} {profiled_points[wc]} \n"
+
+    for wc in wc_names:
+        for i in scan_points: 
+            if wc=='ctGRe' or wc=='ctGIm':
+                i = i/10.0
+            rwgtCards += f"launch -n {wc}={i}\n"
+
+            for j in wc_names: 
+                if wc == j: 
+                    rwgtCards += f"set param_card {j} {i} \n"
+                else: 
+                    rwgtCards += f"set param_card {j} {profiled_points[wc]} \n"
+
+    fname = f"{outname}_profiled.dat"
+    open(fname, 'wt').write(rwgtCards)
+    print(f"card saved to: {fname}")
+
+
 def make_card_frozen(MG_dir, wc_names, scan_points, outname):
+
+    print("Creating card with WCs frozen to SM.")
     
     rwgtCards = ''
     rwgtCards += f"launch {MG_dir} \n"
@@ -24,7 +53,7 @@ def make_card_frozen(MG_dir, wc_names, scan_points, outname):
                 else: 
                     rwgtCards += f"set param_card {j} 0.0 \n"
 
-    fname = f"{outname}.dat"
+    fname = f"{outname}_frozen.dat"
     open(fname, 'wt').write(rwgtCards)
     print(f"card saved to: {fname}")
 
@@ -43,5 +72,9 @@ if __name__ == "__main__":
     MG_dir = options['MG_dir']
     wc_names = options['wc_names']
     scan_points = options['scan_points']
+    if 'profiled_points' in options and len(options['profiled_points'])!=0:
+        profiled_points = options['profiled_points']
+        make_card_profiled(MG_dir, wc_names, scan_points, outname, profiled_points)
+
 
     make_card_frozen(MG_dir, wc_names, scan_points, outname)
