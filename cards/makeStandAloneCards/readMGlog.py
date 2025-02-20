7dedfb1b6f9086b48149018164cc9e358ebee664
hannahbnelson <hnelson2@nd.edu>
2025-02-20 11:44:59 -0500

    working scripts to create MG stand alone .dat files, and then parse the output logs into a text file usable for 1D quad plotting script

--- cards/makeStandAloneCards/readMGlog.py
+++ cards/makeStandAloneCards/readMGlog.py
@@ -6,14 +6,24 @@ def skim_MGlog(inFile, outFile):
     with open(outFile, "w") as output: 
         with open(inFile, "r") as input: 
             for line in input:
+                
                 if "Summary" in line.strip("\n"):
-                    line=line.strip("  === Results Summary for run:")
-                    line=line.strip("tag: tag_1 ===")
+                    ### strip and lstrip also remove "S" from the SM point name so I switched to the .find method
+                    ### line=line.strip("tag: tag_1 ===") works interactively but not when run in the script 
+                    ### I have no clue why so instead just use line.find
+                    # line=line.lstrip("=== Results Summary for run:")
+                    # line=line.rstrip("tag: tag_1 ===")
+
+                    index=line.find("run:")+5   #find index for first character after "Results Summary for run: "
+                    line=line[index:]           #shorten line to remove those characters
+                    line=line[:line.find("tag")]+'\n'   #remove "tag: tag_1 ===" from the end
                     output.write(line)
+
                 if "Cross-section :" in line.strip("\n"):
                     line=line.strip("     Cross-section :   ")
                     output.write(line)
 
+    print(f"saving to {outFile}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Command Line options parser")
