dc69b6d7c70582b32ed37097c1f5a3d0dec0c1c0
Hannah Nelson <hnelson2@nd.edu>
2025-06-16 14:51:22 -0400

    finished cards for tWLO gridpack

--- cards/check_rwgt_distribution.py
+++ cards/check_rwgt_distribution.py
@@ -3,8 +3,10 @@ from hist import Hist
 import mplhep as hep
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+import argparse
+import os
 
-def read_reweightcard(fname):
+def read_reweightcard(fname, outname):
     '''
     Return a dictionary of xsec values at different WC points. 
 
@@ -12,6 +14,8 @@ def read_reweightcard(fname):
     ---------- 
     fname : str
         file name (and if not in the same directory, the path) of the reweight card
+    outname: str
+        output file name
     '''
     
     with open(fname, "r") as f: 
@@ -23,7 +27,7 @@ def read_reweightcard(fname):
     for line in lines: 
         if "set param_card" in line: 
             line = line[19:]
-            tmp = line.split(" ")f
+            tmp = line.split(" ")
 
             wc_name = tmp[0]
             wc_val = float(tmp[1])
@@ -38,7 +42,7 @@ def read_reweightcard(fname):
         hists[wc]=Hist(hist.axis.Regular(bins=50, start=min(rwgt_values[wc]), stop=max(rwgt_values[wc]), name=wc, label=wc))
         hists[wc].fill(rwgt_values[wc])
 
-    with PdfPages('rwgt_values.pdf') as pdf:
+    with PdfPages(f"{outname}.pdf") as pdf:
         for wc in rwgt_values.keys():
             fig, ax = plt.subplots()
             hep.histplot(hists[wc], ax=ax, stack=False, yerr=False, label=wc)
@@ -48,5 +52,14 @@ def read_reweightcard(fname):
             pdf.savefig(fig)
 
 if __name__ == '__main__':
-    read_reweightcard("/afs/crc.nd.edu/user/h/hnelson2/unpack_tWgridpack/InputCards/tW_OGstpt_reweight_card.dat")
+    parser = argparse.ArgumentParser(description="Command Line options parser")
+    parser.add_argument("file", help="path to reweight card")
+    parser.add_argument("--outname", default="rwgt_hists", help="Output file name")
+    args = parser.parse_args()
+
+    filename = args.file
+    outname = args.outname 
+
+    read_reweightcard(filename, outname)
+    print(f"plots saved to {outname}.pdf")
 
